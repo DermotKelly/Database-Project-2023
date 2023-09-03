923a234d87eaa628265347d5462895e3bfe7334e
Dermot Kelly <mrdermotkelly@gmail.com>
2023-09-03 21:31:24 +0100

    Third commit

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import numpy as np
 #import os
 from config import Config
--- config.py
+++ config.py
@@ -5,3 +5,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     
+    
+    #f0070b9d436a04dc77d6337eb127638b
+    
